6a00f0169d1b15a31bb32c896602c9e8f9a77b85
Treinamento <Treinamento@RD723913.raiadrogasil.com>
2020-02-13 14:30:17 -0300

    atualizacao dos arquivos

--- src/exercicio01/Caneta.java
+++ src/exercicio01/Caneta.java
@@ -1,4 +1,49 @@
 package exercicio01;
 
 public class Caneta {
+    private String cor;
+    private String tampa;
+    private double valor;
+
+    public Caneta(){
+
+    }
+
+    public Caneta (String cor, String tampa, double valor){
+        this.cor = cor;
+        this.tampa = tampa;
+        this.valor = valor;
+    }
+
+    public void escrever(){
+        System.out.println("Caneta Escrevendo");
+    }
+
+    public void pintar(){
+        System.out.println("pintando");
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTampa() {
+        return tampa;
+    }
+
+    public void setTampa(String tampa) {
+        this.tampa = tampa;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
--- src/exercicio01/exercicio01.java
+++ src/exercicio01/exercicio01.java
@@ -1,4 +1,14 @@
 package exercicio01;
 
 public class exercicio01 {
+    public static void main(String[] args) {
+    Caneta canetinha = new Caneta("azul", "azul", 3.50);
+     Caneta canetinha2 = new Caneta("vermelha", "preta", 3.50);
+     Caneta canetinha3 = new Caneta("verde", "vermelha", 3.50);
+
+     canetinha.escrever();
+     canetinha2.pintar();
+     canetinha3.escrever();
+     canetinha3.pintar();
+    }
 }
